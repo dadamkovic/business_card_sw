8abae0247d9e665e8688fa3cb5bf0cbf658103de
Daniel <dadamkovic@gmail.com>
2023-09-30 16:25:48 -0400

    Initial commit with only bare generated data from CubeMx

---
+++ build/game_control.d
@@ -0,0 +1,6 @@
+build/game_control.o: Core/Src/game_control.c Core/Inc/game_control.h \
+ Core/Inc/defines.h Core/Inc/shape_moves.h Core/Inc/game_control.h
+Core/Inc/game_control.h:
+Core/Inc/defines.h:
+Core/Inc/shape_moves.h:
+Core/Inc/game_control.h:
---
+++ build/shape_moves.d
@@ -0,0 +1,5 @@
+build/shape_moves.o: Core/Src/shape_moves.c Core/Inc/shape_moves.h \
+ Core/Inc/defines.h Core/Inc/game_control.h
+Core/Inc/shape_moves.h:
+Core/Inc/defines.h:
+Core/Inc/game_control.h:
